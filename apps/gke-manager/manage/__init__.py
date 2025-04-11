7d9e6bf05bebd46a8b26d76566f9e7bfcfd401ca
Syed Islamuddin Shah <syedislamuddinshah@Syeds-MacBook-Pro.local>
2025-04-11 16:11:26 -0400

    gke-version

--- apps/gke-manager/manage/__init__.py
+++ apps/gke-manager/manage/__init__.py
@@ -14,9 +14,6 @@ secret_value_json = response.payload.data.decode("UTF-8")
 # Load the JSON string into a dictionary
 secret_value_dict = json.loads(secret_value_json)
 
-# Oauth_CLIENT_ID="664722061460-qta0d2bf6c5h000s23m11oc0tmaf0h54.apps.googleusercontent.com"
-# Oauth_CLIENT_SECRET="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"
-
 os.environ["CLIENT_ID"] = secret_value_dict['Oauth_CLIENT_ID']
 os.environ["CLIENT_SECRET"] = secret_value_dict['Oauth_CLIENT_SECRET']
 os.environ["REDIRECT_URI"] = secret_value_dict['Oauth_REDIRECT_URI']
